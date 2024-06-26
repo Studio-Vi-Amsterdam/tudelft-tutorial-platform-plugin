ad703b80b606f92c543d00eb17c3abfe129b0ab8
AljosaK <koturaljosa@yahoo.com>
2024-06-26 16:31:32 +0200

    Allowing DELETE method

--- include/common/class-rest-api.php
+++ include/common/class-rest-api.php
@@ -59,7 +59,7 @@ class Rest_Api {
         header( 'Access-Control-Allow-Origin: *' );
 
         // Allow the following HTTP methods
-        header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
         // Allow the following headers to be included in the request
         header("Access-Control-Allow-Headers: Content-Type, Authorization");

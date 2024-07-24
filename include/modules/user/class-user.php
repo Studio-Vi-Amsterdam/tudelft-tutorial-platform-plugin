ff852b4426dd076bb81fb8f8c9d8b64cdaa2189c
AljosaK <koturaljosa@yahoo.com>
2024-07-24 10:47:58 +0200

    Ability for subscribers to preview post

--- include/modules/user/class-user.php
+++ include/modules/user/class-user.php
@@ -6,4 +6,19 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 class User {
 
+    function __construct() {
+        add_action( 'admin_init', [ $this, 'add_custom_preview_capability' ] );
+    }
+
+    /**
+     * Add custom preview capability
+     * 
+     * @since 1.0.0
+     * 
+     * @return void
+     */
+    public function add_custom_preview_capability() {
+        $role = get_role( 'subscriber' );
+        $role->add_cap( 'edit_posts' );
+    }
 }
